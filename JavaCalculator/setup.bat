308aa2415ac8cec2e4bd7b230b571d177dabd32a
Ryan Parlin <parlinr@ad.nmc.edu>
2018-02-01 10:07:36 -0500

    start working on lab version

--- JavaCalculator/setup.bat
+++ JavaCalculator/setup.bat
@@ -3,6 +3,6 @@ rem This file configures the settings necessary to compile the lab project on th
 rem However, this file needs to be changed in a couple of places.
 rem You need to replace all instances of <your NMC ID> with your NMC ID, otherwise
 rem this batch file will not run and you will not be able to compile the test project.
-set ANT_HOME=C:\Users\parlinr\Desktop\Java-calculator\JavaCalculator\apache-ant-1.10.1-bin\apache-ant-1.10.1
+set ANT_HOME=C:\Users\<your NMC ID>\Desktop\Java-calculator\JavaCalculator\apache-ant-1.10.1-bin\apache-ant-1.10.1
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_151
 set PATH=%PATH%;%ANT_HOME%\bin  
